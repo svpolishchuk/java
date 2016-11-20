9384ccefca3f22a70200cf3cdcc34c34eb5e4b1b
Serhii <polishchuk_sv@ukr.net>
2016-11-20 13:05:29 +0200

    New Class LitecartAdminLogin - HW

--- src/test/java/LitecartAdminLogin.java
+++ src/test/java/LitecartAdminLogin.java
@@ -9,27 +9,26 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 /**
- * Created by PSV_LaDevelopers on 17.11.2016.
+ * Created by polis on 19.11.2016.
  */
-public class MyFirstTest {
+public class LitecartAdminLogin {
 
     private WebDriver driver;
     private WebDriverWait wait;
 
-
     @Before
     public void start(){
-        System.setProperty("webdriver.chrome.driver", "C:\\QASeleniumJava\\chromedriver.exe");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void myFirstTest(){
-        driver.get("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
-        wait.until(titleIs("webdriver - Пошук Google"));
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.findElement(By.className("logotype")).click();
     }
 
     @After
