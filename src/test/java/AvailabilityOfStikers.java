437d5a98c2ccd9696b42c9c98883dc46f96ac759
Serhii Polishchuk <polishchuk_sv@ukr.net>
2016-12-05 15:25:14 +0200

    Task8. Make a script that checks the presence of stickers in goods

---
+++ src/test/java/AvailabilityOfStikers.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * Created by PSV_LaDevelopers on 05.12.2016.
+ */
+public class AvailabilityOfStikers extends TestBase {
+
+    @Test
+    public void Availability(){
+
+        driver.get("http://google.com");
+
+
+
+    }
+
+}
